79c2e6afcb70e5c47543a48409b19e2e2c29c6a7
Batuhan Eraslan <bthneraslan@hotmail.com>
2024-05-22 20:42:11 +0300

    Docker MSSQL ayağa kaldırılması

--- Services/Order/Infrastructre/MultiShop.Order.Persistence/Context/OrderContext.cs
+++ Services/Order/Infrastructre/MultiShop.Order.Persistence/Context/OrderContext.cs
@@ -3,11 +3,11 @@ using MultiShop.Order.Domain.Entities;
 
 namespace MultiShop.Order.Persistence.Context
 {
-    public class OrderContext:DbContext
+    public class OrderContext : DbContext
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server= ; initial Catalog=MultiShopOrderDb; integrated Security = true;");
+            optionsBuilder.UseSqlServer("Server=localhost,1440; initial Catalog=MultiShopOrderDb; User=sa; Password=123456789Aa.");
         }
         public DbSet<Address> Addresses { get; set; }
         public DbSet<OrderDetail> OrderDetails { get; set; }
