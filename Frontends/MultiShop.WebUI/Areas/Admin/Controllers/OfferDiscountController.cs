088e26e435a63565b311dcc84fd9cb918b88751e
Batuhan Eraslan <108789207+bthneraslan@users.noreply.github.com>
2024-11-23 17:12:39 +0300

    Offer Discount and Brand

---
+++ Frontends/MultiShop.WebUI/Areas/Admin/Controllers/OfferDiscountController.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.OfferDiscountDtos;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace MultiShop.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/OfferDiscount")]
+    public class OfferDiscountController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public OfferDiscountController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        [Route("Index")]
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.v1 = "Ana sayfa";
+            ViewBag.v2 = "Özel Teklifler";
+            ViewBag.v3 = "Özel Teklif Listesi";
+            ViewBag.v0 = "Özel Teklif İşlemleri";
+
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:7070/api/OfferDiscount");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultOfferDiscountDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        [HttpGet]
+        [Route("CreateOfferDiscount")]
+        public IActionResult CreateOfferDiscount()
+        {
+            ViewBag.v1 = "Ana sayfa";
+            ViewBag.v2 = "Özel Teklifler";
+            ViewBag.v3 = "Yeni Özel Teklif Ekleme";
+            ViewBag.v0 = "Özel Teklif İşlemleri";
+            return View();
+        }
+        [HttpPost]
+        [Route("CreateOfferDiscount")]
+        public async Task<IActionResult> CreateOfferDiscount(CreateOfferDiscountDto createOfferDiscountDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createOfferDiscountDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("http://localhost:7070/api/OfferDiscount", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "OfferDiscount", new { area = "Admin" });
+            }
+
+            return View();
+        }
+
+        [Route("DeleteOfferDiscount/{id}")]
+        public async Task<IActionResult> DeleteOfferDiscount(string id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync("http://localhost:7070/api/OfferDiscount?id=" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "OfferDiscount", new { area = "Admin" });
+            }
+
+            return View();
+        }
+        [Route("UpdateOfferDiscount/{id}")]
+        [HttpGet]
+        public async Task<IActionResult> UpdateOfferDiscount(string id)
+        {
+            ViewBag.v1 = "Ana sayfa";
+            ViewBag.v2 = "Özel Teklifler";
+            ViewBag.v3 = "Özel Teklif Güncelleme";
+            ViewBag.v0 = "Özel Teklif İşlemleri";
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:7070/api/OfferDiscount/" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateOfferDiscountDto>(jsonData);
+                return View(values);
+            }
+            return View();
+
+        }
+        [Route("UpdateOfferDiscount/{id}")]
+        [HttpPost]
+        public async Task<IActionResult> UpdateOfferDiscount(UpdateOfferDiscountDto updateOfferDiscountDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateOfferDiscountDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("http://localhost:7070/api/OfferDiscount/", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "OfferDiscount", new { area = "Admin" });
+            }
+            return View();
+
+        }
+    }
+}
--- Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/_FeatureProductsDefaultComponentPartial.cs
+++ Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/_FeatureProductsDefaultComponentPartial.cs
@@ -1,11 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.ProductDtos;
+using Newtonsoft.Json;
+using System.Net.Http;
 
 namespace MultiShop.WebUI.ViewComponents.DefaultViewComponents
 {
     public class _FeatureProductsDefaultComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _FeatureProductsDefaultComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:7070/api/Products");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
